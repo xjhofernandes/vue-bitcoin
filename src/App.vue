301074b6f0f2ec3f826635797e88e56ccf2bde4b
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2021-01-02 01:58:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="head">
+  <!-- <img class="as" alt="Vue" src="./assets/bitcoin.svg"> -->
+  <p>Bitcoin Home</p> 
+</div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <div class="container">
+    <img class="bitcoin" alt="Vue" src="./assets/Investing.svg">
+    <HelloWorld />
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 0px;
+} */
+
+.head{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 85px;
+  margin-bottom: 60px;
+  color: white;
+  background: black;
+}
+
+.head p{
+  font-size: 2em;
+  color: white;
+}
+
+.container{
+  /* background: red; */
+  padding-left: 15%;
+  padding-right: 15%;
+}
+
+.bitcoin{
+  height: 24em;
+  color: #2e86c1;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="chart">
+    <apexchart
+      width="600"
+      type="area"
+      :options="chart.chartOptions"
+      :series="chartSerie.series"
+    ></apexchart>
+    <div>
+      <button @click="updateChart">Update!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+import { reactive } from 'vue';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    apexchart: VueApexCharts,
+  },
+  setup(){
+    const chart = reactive({
+      chartOptions: {
+        chart: {
+          id: "vuechart-example",
+          type: 'area',
+          stacked: false,
+          height: 400,
+          zoom: {
+            enabled: false
+          },          
+        },
+        dataLabels: {
+          enabled: false
+        },
+        markers:{
+          size: 0
+        },
+        fill: {
+          type: 'gradient',
+          gradient: {
+            shadeIntensity: 1,
+            inverseColors: false,
+            opacityFrom: 0.45,
+            opacityTo: 0.05,
+            stops: [20, 100, 100, 100]            
+          }
+        },
+        xaxis: {
+          categories: [1578798000000,1581476400000,1583982000000,1586660400000,1589252400000,1591930800000,1594522800000,1597201200000],
+          type: 'datetime',
+          axisBorder: {
+            show: false},
+          axisTicks: {
+            show: false},
+          tooltip: {
+              shared: true},
+          legend: {
+            position: 'top',
+            horizontalAlign: 'right',
+            offsetX: -10
+          },        
+        },
+        yaxis: {
+          labels: {
+            formatter: function (value) {
+              return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'BRL' }).format(value);
+            },
+          },
+        },
+      },
+    });
+    
+    const chartSerie = reactive({
+      series: [
+        {
+          name: "Bitcoin",
+          data: [30, 40, 45, 50, 49, 60, 70, 99],
+        }
+      ],
+    });
+
+    async function updateChart() {
+      let valores = await minhasbits();
+
+      let bitcointValues = valores.map(function(valor) {return valor['closing']});
+      let bitCoinDate = valores.map(function(valor) {return Date.parse(valor['date'])});
+
+      const colors = ["#16a085"]; 
+
+      chart.chartOptions = {
+        colors: [colors[Math.floor(Math.random() * colors.length)]],
+        xaxis:{
+          categories : bitCoinDate
+        }
+      };
+    
+      chartSerie.series = [
+        {
+          data: bitcointValues,
+        },
+      ];
+    }
+
+    async function minhasbits(){
+      let bitcoins = [];
+      for (var i = 1; i <= 31; i++) {
+            let a = await fetch(`https://www.mercadobitcoin.net/api/BTC/day-summary/2020/12/${i}`)
+            .then(function(response) {
+            return response.json();
+            });
+            bitcoins.push(a)
+      }
+      return bitcoins
+    }
+
+    return {updateChart, minhasbits, chart, chartSerie}
+  }
+}
+</script>
+
+<style scoped>
+
+.chart{
+  float: right;
+  background: white;
+}
+
+</style>
