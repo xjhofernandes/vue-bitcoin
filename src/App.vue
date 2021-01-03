faa56bab472f72e693e156f9a8fb47ce526ddaf5
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2021-01-03 19:00:01 -0300

    "refactoring"

--- src/App.vue
+++ src/App.vue
@@ -8,33 +8,30 @@
         <h2 class="subtitle">
           A simple site to check the bitcoin price
         </h2 >
-        
       </div>
     </div>
   </section>
 
   <div class="columns p-6">
-
     <div class="column is-two-fifths">
       <p class="is-primary">
         <img class="" id="svg" alt="Vue" src="./assets/Investing.svg">
       </p>
     </div>
-    
     <div class="column">
       <HelloWorld />
     </div>
   </div>
 
-<footer class="footer has-background-dark">
-  <div class="content has-text-centered">
-    <p class="has-text-white">
-      <strong class="has-text-white">Site</strong> by <a href="https://jhofernandes.dev">Jonathan Fernandes</a>. The source code is licensed
-      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-      Made in Vue + Bulma + FastApi
-    </p>
-  </div>
-</footer>
+  <footer class="footer has-background-dark">
+    <div class="content has-text-centered">
+      <p class="has-text-white">
+        <strong class="has-text-white">Site</strong> by <a href="https://jhofernandes.dev">Jonathan Fernandes</a>. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        Made in Vue + Bulma + FastApi
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -53,5 +50,4 @@ export default {
   font-family: "Roboto";
   font-weight: 400;
 }
-
 </style>
