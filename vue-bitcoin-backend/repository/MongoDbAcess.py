c775932ae1212b0f456f03f17cd1f9f640a4ecc2
Jonathan Fernandes <jhofernandes@pop-os.localdomain>
2021-01-04 14:35:48 -0300

    adding python backend first files

---
+++ vue-bitcoin-backend/repository/MongoDbAcess.py
@@ -0,0 +1,3 @@
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://admin:bitadmin@cluster0.oyaw3.mongodb.net/test?authSource=admin&replicaSet=atlas-ai8to1-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
