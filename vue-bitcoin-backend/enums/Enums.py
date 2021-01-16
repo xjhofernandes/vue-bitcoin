173a46ed0477a6a59a673acbe287aef03b97cbf1
Jonathan Fernandes <46504678+xjhofernandes@users.noreply.github.com>
2021-01-16 12:33:57 -0300

    Deleting backend folder.
    Backend repository: https://github.com/xjhofernandes/vue-bitcoin-backend

--- vue-bitcoin-backend/enums/Enums.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-from datetime import timedelta
-
-class Periods(Enum):
-   seven = timedelta(days = 7)
-   month = timedelta(days = 30)
-   sixmonths = timedelta(days = 180)
-   year = timedelta(days = 360)
-   max = timedelta(days = 999)
--- vue-bitcoin-backend/model/RequestBody.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class RequestBody(BaseModel):
-    period: str = None
-    date_start: datetime = None
-    date_end: datetime = None
